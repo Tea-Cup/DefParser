7b5994f5790c310b2688374b26d18386c7ad0123
Tea-Cup <cozyteacup@pm.me>
2024-05-26 06:56:55 +0700

    Better debug content path detection

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using DefParser.Defs;
 
 namespace DefParser {
 	internal class Program {
 		static void Main(string[] args) {
 			Def.MultilineToString = true;
-			string root = @$"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\source\repos\DefParser\content\";
-			string defs = Path.Combine(root, "defs");
-			string assets = Path.Combine(root, "assets");
+			string root = File.ReadAllText("SolutionDir.env").Trim();
+			string content = Path.Combine(root, "content");
+			string defs = Path.Combine(content, "defs");
+			string assets = Path.Combine(content, "assets");
 			Logger.Write("");
 			Parser.ParseDefs(defs, new[] { assets });
 			foreach (Def def in DefDatabase<Def>.All) {
